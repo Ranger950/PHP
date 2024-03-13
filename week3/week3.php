30bbbcc7d5394ddb769e3d7de6bd8c3a9523bed5
Ranger950 <125946506+Ranger950@users.noreply.github.com>
2024-03-13 23:36:39 +0800

    Add files via upload

---
+++ week3/week3.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>履歷表</title>
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        text-align: center; 
+    }
+    h1, h2 {
+        text-align: center;
+    }
+    table {
+        margin: 0 auto;
+        border-collapse: collapse;
+        width: 50%; 
+    }
+    th, td {
+        padding: 8px;
+        border: 1px solid black;
+        text-align: center; 
+    }
+</style>
+</head>
+<body>
+<h1>謝祁翰</h1>
+<center><img src="IMG_4034.png" width="200" height="300" alt="照片"></center>
+
+<hr>
+<p><h2>簡介</h2>
+    <b>家鄉：</b>高雄市<br>
+    <b>生日：</b>2004/03/31<br>
+    <b>電話：</b>09**-***-***<br>
+    <b>信箱：</b>qwe20040331@gmail.com<br>
+</p>
+
+<table>
+    <thead>
+        <tr>
+            <th>休閒活動</th>
+            <th>個性</th>
+            <th>興趣</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>聽搖滾樂、看電影、打拳擊、健身</td>
+            <td>叛逆、理性</td>
+            <td>打拳擊、打電腦、收集模型</td>
+        </tr>
+    </tbody>
+</table>
+
+<hr>
+
+<h2>自我介紹</h2>
+<p><font color="black">
+    我喜歡聆聽搖滾樂，讓音樂陪伴我的情緒波動。<br>
+    我也熱愛觀賞各種類型的電影，從經典到科幻，喜歡電影在我人生中不同階段中帶來的啟發和感受。<br>
+    打拳擊跟健身是我近來生活中不可或缺的一部分，透過鍛煉身體，培養自律和毅力，並在挫折時有個地方可以發洩。<br>
+    我的個性叛逆而理性，我喜歡挑戰傳統觀念，但同時也能保持冷靜和理智。在探索新事物時保持著平衡和理性思考。<br>
+    除此之外，我對打電腦和收集模型也很感興趣。打電腦讓我能夠進入到一個不同的世界與認識更多的人，而收集模型則能讓我透過物品，去記錄我在當時階段的想法與喜愛的藝術作品。</font></p>
+
+</body>
+</html>
